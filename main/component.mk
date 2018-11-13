a207cd1d2535ff6df7e6bd2dc73ceb0e5c60f759
DUVILLIE Guillerme <guillerme@duvillie.eu>
2018-11-13 12:05:36 +0100

    Generate config.h from config.def.h

--- main/component.mk
+++ main/component.mk
@@ -2,3 +2,11 @@
 # "main" pseudo-component makefile.
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
+
+
+morma.o: config.h
+
+escreen_print.o: config.h
+
+config.h: $(COMPONENT_PATH)/config.def.h
+	cp $(COMPONENT_PATH)/config.def.h $(COMPONENT_PATH)/config.h
